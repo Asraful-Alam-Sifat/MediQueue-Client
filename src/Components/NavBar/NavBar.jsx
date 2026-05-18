579eaad13c0b212684a9dedc69c9dd1967558293
Asraful Alam <asrafulalam.origin@gmail.com>
2026-05-18 13:57:58 +0600

    add navbar and make it responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,49 +1,119 @@
-import Logo from '@/assets/mediqueue-logo.png'
-import Image from 'next/image';
-import Link from 'next/link';
+import Logo from "@/assets/mediqueue-logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
-    return (
-         <div className="navbar sticky top-0 z-50 border-b bg-transparent border-white/5  sm:px-10 py-4 flex-col sm:flex-row justify-between gap-y-4 "
-  style={{ background: 'rgba(12, 12, 16, 0.85)', backdropFilter: 'blur(16px)' }}>
-  <div className="navbar-start w-full sm:w-auto flex flex-row items-center justify-center sm:justify-start gap-2 ">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <Link href={'#tutors'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>tutors</li></Link>
-        <Link href={'#add-tutor'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>add tutor</li></Link>
-        <Link href={'#my-tutors'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'> <li>my tutors</li></Link>
-        <Link href={'#sessions'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>my sessions</li></Link>
-        
-       
-        
-      </ul>
-    </div>
-    
-        <Link href={'#home'} className='flex items-end-safe gap-1'>
-        <Image src={Logo} alt='logo' className='w-12'/>
-        <h1 className='font-serif font-extrabold text-4xl text-[#1B54A4] '>Medi<span className='text-emerald-700'>Queue</span></h1>
+  return (
+    <div
+      className="navbar sticky top-0 z-50 border-b bg-transparent border-white/5  sm:px-10 py-4 flex-col sm:flex-row gap-y-4 justify-between "
+      style={{
+        background: "rgba(12, 12, 16, 0.85)",
+        backdropFilter: "blur(16px)",
+      }}
+    >
+      <div className="navbar-start w-full sm:w-auto flex flex-row items-center justify-center sm:justify-start gap-2 ">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <Link
+              href={"#tutors"}
+              className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+            >
+              <li>tutors</li>
+            </Link>
+            <Link
+              href={"#add-tutor"}
+              className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+            >
+              <li>add tutor</li>
+            </Link>
+            <Link
+              href={"#my-tutors"}
+              className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+            >
+              {" "}
+              <li>my tutors</li>
+            </Link>
+            <Link
+              href={"#sessions"}
+              className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+            >
+              <li>my sessions</li>
+            </Link>
+          </ul>
+        </div>
+
+        <Link href={"#home"} className="flex items-end-safe gap-1">
+          <Image src={Logo} alt="logo" className="w-12" />
+          <h1 className="font-serif font-extrabold text-4xl text-[#1B54A4] ">
+            Medi<span className="text-emerald-700">Queue</span>
+          </h1>
         </Link>
-    
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal space-x-10 px-1 text-xl">
-       <Link href={'#tutors'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>tutors</li></Link>
-        <Link href={'#add-tutor'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>add tutor</li></Link>
-        <Link href={'#my-tutors'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'> <li>my tutors</li></Link>
-        <Link href={'#sessions'} className='text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize'><li>my sessions</li></Link>
-    </ul>
-  </div>
-  <div className="navbar-end w-full sm:w-auto flex flex-row items-center justify-center sm:justify-end gap-3">
-    <Link href={''} className=' text-emerald-950 font-bold text-lg p-2 rounded-md bg-emerald-600 hover:bg-[#1A1A1A] hover:border-2 hover:border-emerald-900 hover:text-emerald-600 capitalize'>sign in</Link>
-    <Link href={''} className=' text-emerald-950 font-bold text-lg p-2 rounded-md bg-emerald-600 hover:bg-[#1A1A1A] hover:border-2 hover:border-emerald-900 hover:text-emerald-600 capitalize'>register free</Link>
-  </div>
-</div>
-    );
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal space-x-10 px-1 text-xl">
+          <Link
+            href={"#tutors"}
+            className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+          >
+            <li>tutors</li>
+          </Link>
+          <Link
+            href={"#add-tutor"}
+            className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+          >
+            <li>add tutor</li>
+          </Link>
+          <Link
+            href={"#my-tutors"}
+            className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+          >
+            {" "}
+            <li>my tutors</li>
+          </Link>
+          <Link
+            href={"#sessions"}
+            className="text-white/45  hover:text-white hover:opacity-95 transition-all duration-150 text-xl capitalize"
+          >
+            <li>my sessions</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="navbar-end w-full sm:w-auto flex flex-row items-center justify-center sm:justify-end gap-3">
+        <Link
+          href={""}
+          className=" text-emerald-950 font-bold text-lg p-2 rounded-md bg-emerald-600 hover:bg-[#1A1A1A] hover:border-2 hover:border-emerald-900 hover:text-emerald-600 capitalize"
+        >
+          sign in
+        </Link>
+        <Link
+          href={""}
+          className=" text-emerald-950 font-bold text-lg p-2 rounded-md bg-emerald-600 hover:bg-[#1A1A1A] hover:border-2 hover:border-emerald-900 hover:text-emerald-600 capitalize"
+        >
+          register free
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
